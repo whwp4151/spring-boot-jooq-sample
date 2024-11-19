20489d264a3057c5e2dbff506b4abb4959c1bcd0
whwp4151 <whwp4151@naver.com>
2024-11-19 10:32:51 +0900

    first commit

---
+++ src/main/java/com/example/spring_boot_jooq_sample/SpringBootJooqSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jooq_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJooqSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJooqSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_jooq_sample/SpringBootJooqSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jooq_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJooqSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
